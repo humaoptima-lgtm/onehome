4a78b1ee126e4bed8fd30b91d272a152b229b38c
mfaishal82 <mfaishal82@yahoo.co.id>
2026-06-03 14:41:19 +0700

    Fix: remove build artifacts from git add to gitignore

--- apps/web/.open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"hostinger_password":"o-LcNX.;.3d1F+mq"};
-export const development = {"hostinger_password":"o-LcNX.;.3d1F+mq"};
-export const test = {"hostinger_password":"o-LcNX.;.3d1F+mq"};
