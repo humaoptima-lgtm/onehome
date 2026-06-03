5f0f1a066f44c14a320c5bedcb3bbf98a8dc44cb
mfaishal82 <mfaishal82@yahoo.co.id>
2026-06-03 14:37:10 +0700

    Fix: remove broken cp commands...

---
+++ apps/web/.open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"hostinger_password":"o-LcNX.;.3d1F+mq"};
+export const development = {"hostinger_password":"o-LcNX.;.3d1F+mq"};
+export const test = {"hostinger_password":"o-LcNX.;.3d1F+mq"};
